18709e3b7bc4b47f9c3c71a7804390601919b2fd
Hamzah-Ahmad <ahmadhamzah500@gmail.com>
2019-08-16 15:41:53 -0500

    final version with all features

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <div className="App">
       <Provider store = {store}>
-        <Counter />
+        <Counter/>
       </Provider>
     </div>
   );
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -5,13 +5,11 @@ const initialState = {
 const rootReducer = (state = initialState, action) => {
     switch(action.type){
         case 'INCREMENT':
-            console.log("Increment. State is " +state.count)
             return{
                 count: state.count + 1
                 
             };
         case 'DECREMENT':
-            console.log("Decrement. State is " +state.count)
 
             return{
                 
